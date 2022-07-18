62dd1303e4a2fb7cffb1bab12e0766050981a2ee
Mosina916 <mosinaashraf43@gmail.com>
2022-07-18 21:42:35 +0530

    recursion

--- Lecture18/multoftwonumbers.cpp
+++ Lecture18/multoftwonumbers.cpp
@@ -13,9 +13,35 @@ int multiplicationoftwonumbers(int a,int b){ //0 0
 
 int main(){
 	int a,b;
-	cin>>a>>b;//0 3
+	cin>>a>>b;// -5 -3
+		// cout<<multiplicationoftwonumbers(a,b)<<endl;
+	// both postive
+	if(a>=0 and b>=0){
+		cout<<multiplicationoftwonumbers(a,b)<<endl;
 
-	cout<<multiplicationoftwonumbers(a,b)<<endl;
+	}
+	// a +ive b -ve
+	else if(a>=0 and b<0){
+		b=abs(b);//4
+		cout<<-multiplicationoftwonumbers(a,b)<<endl;
+
+	}
+	// a -ve b +ive
+	else if(a<0 and b>=0){
+			a=abs(a);//5
+		cout<<-(multiplicationoftwonumbers(a,b))<<endl;
+
+	}
+	else{
+		b=abs(b);//5
+		a=abs(a);//3
+
+		cout<<multiplicationoftwonumbers(a,b)<<endl;
+
+	}
+
+
+	
 	
 
 
