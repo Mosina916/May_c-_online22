8a08c391c9043347bc93cd60ec4ae626b11937a2
Mosina916 <mosinaashraf43@gmail.com>
2022-08-23 12:18:29 +0530

    oops and vectors

--- Lecture8/bubblesort.cpp
+++ Lecture8/bubblesort.cpp
@@ -8,46 +8,28 @@ int main(){
 	// // int arr[1000];
 
 
-	// cout<<"before sorting "<<endl;
-	// for(int i=0;i<=n-1;i++){
-	// 	cout<<arr[i]<<" ";
-	// }
-	// cout<<endl;
+	cout<<"before sorting "<<endl;
+	for(int i=0;i<=n-1;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
 
 	// bubble sort
 
-	// for(int i=0;i<=n-2;i++){
-	// 	for(int j=0;j<=n-2-i;j++){
-	// 	if(arr[j]>arr[j+1]){
-	// 		// swap(arr[j],arr[j+1]);
+	for(int i=0;i<=n-2;i++){
+		for(int j=0;j<=n-2-i;j++){
+		if(arr[j]>arr[j+1]){
+			swap(arr[j],arr[j+1]);
 
-	// 		// using 3rd variable
-	// 		// int temp=arr[j];
-	// 		// arr[j]=arr[j+1];
-	// 		// arr[j+1]=temp;
+			
 
+		}
 
-	// 		// without using 3rd variable
-	// 		// arr[j]=arr[j]+arr[j+1];
-	// 		// arr[j+1]=arr[j]-arr[j+1];
-	// 		// arr[j]=arr[j]-arr[j+1];
+	}
 
 
-	// 		//inbuild swap
-	// 		// swap(arr[j],arr[j+1]);
-	// 		// swap(arr[j+1],arr[j]);
-
-
-	// 		// on line swap
-	// 		arr[j+1]=(arr[j]+arr[j+1])-(arr[j]=arr[j+1]);
-
-
-	// 	}
-
-	// }
-
-
-	// }
+	}
+	
 
 
 // 	for(int i=1;i<=n-1;i++){//i=4
